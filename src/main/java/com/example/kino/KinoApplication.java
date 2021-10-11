a891069b36dd0fa60497417953674283e404c348
Mathias <math8802@stud.kea.dk>
2021-10-11 15:14:40 +0200

    first

---
+++ src/main/java/com/example/kino/KinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kino/KinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
