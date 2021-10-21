76dd2cd7884581f8daf002febcac4db607860bdb
Mathias <math8802@stud.kea.dk>
2021-10-21 14:24:20 +0200

    cleaned up unused stuff

--- src/main/java/com/example/kino/repository/CalendarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kino.repository;
-
-import com.example.kino.model.Calendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CalendarRepository extends JpaRepository<Calendar, Long> {
-}
