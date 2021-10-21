c7c81b6822985577c3c30e592ad5454987191619
Mathias <math8802@stud.kea.dk>
2021-10-21 14:57:42 +0200

    cleaned up unused stuff

--- src/main/java/com/example/kino/service/MovieService.java
+++ src/main/java/com/example/kino/service/MovieService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 
 @Service
-public class MovieService extends Utilities {
+public class MovieService {
 
     private MovieRepository movieRepository;
 
--- src/main/java/com/example/kino/service/Utilities.java
+++
@@ -1,7 +0,0 @@
-package com.example.kino.service;
-
-import java.time.LocalDate;
-
-public abstract class Utilities {
-    public boolean dateChecker(LocalDate startDate, LocalDate endDate) {return endDate.isBefore(startDate);}
-}
